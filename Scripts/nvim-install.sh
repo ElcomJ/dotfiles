ae9debf168871542798a6a14f298876d2728e616
Elçom Junior <elcomj31@gmail.com>
2022-09-22 07:34:02 -0300

    Dotfiles

--- Scripts/nvim-install.sh
+++ Scripts/nvim-install.sh
@@ -10,6 +10,14 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 
 sudo apt-get update
 
-sudo apt-get install neovim
+sudo apt-get -y install neovim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo apt-get -y install universal-ctags
+sudo apt-get -y install xclip
+sudo apt-get -y install ripgrep
+sudo apt-get -y install fd-find
 
 echo "Neovim Installed !"
