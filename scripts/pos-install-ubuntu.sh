f9a766c268998e7d43855a520215a7efbbc10fb5
Elçom Junior <elcomj31@gmail.com>
2022-11-08 18:16:14 -0300

    Dotfiles

---
+++ scripts/pos-install-ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:atareao/telegram
+sudo add-apt-repository ppa:gerardpuig/ppa
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get -y install software-properties-common
+sudo apt-get -y install git
+sudo apt-get -y install htop
+sudo apt-get -y install python-setuptools
+sudo apt-get -y install python3-pip
+sudo apt-get -y install tmux
+sudo apt-get -y install ranger
+sudo apt-get -y install gufw
+sudo apt-get -y install telegram
+sudo apt-get -y install ubuntu-cleaner
+sudo apt-get -y install gnome-tweaks
+sudo apt-get -y install dconf-editor
+sudo apt-get -y install neofetch
+sudo apt-get -y install gnome-software
+sudo apt-get -y install vlc
+sudo apt-get -y install iputils
+sudo apt-get -y install zathura
+
+pip install trash-cli
+
+echo "Pos Install Done !"
