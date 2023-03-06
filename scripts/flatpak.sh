b331513198f51e79e2b3fe07d935599450eba752
Elçom Junior <juninho.rj31@gmail.com>
2023-03-06 12:48:45 -0300

    .config file update

--- scripts/flatpak.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get -y install flatpak
-sudo apt-get -y install gnome-software-plugin-flatpak
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-echo "Flatpak Done !"
